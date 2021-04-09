399ee50a19f9b627fe8edcba951ab4b028984943
BastLast <bastien.last87@gmail.com>
2021-04-09 09:34:15 +0200

    Added date for api call

--- app/src/main/java/com/example/assstriode/HomeActivity.java
+++ app/src/main/java/com/example/assstriode/HomeActivity.java
@@ -3,6 +3,8 @@ package com.example.assstriode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -10,13 +12,20 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.assstriode.model.Asteroid;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Calendar;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+import static com.example.assstriode.R.string.fail_toast;
 import static com.example.assstriode.R.string.sucess_toast;
 
 public class HomeActivity extends AppCompatActivity {
@@ -28,9 +37,11 @@ public class HomeActivity extends AppCompatActivity {
 
         // Instantiate the RequestQueue.​
         RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-        String date = Calendar.getInstance().get(Calendar.YEAR) + "-" + (int) ((int) (Calendar.getInstance().get(Calendar.MONTH)) + 1) + "-" + Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
-        String url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=" + date +
-                "&end_date=" + date + "&api_key=47NucpwWv137CZPlAJZxykgMTMR9KF4bj5TzCYSG";
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date();
+        String today = dateFormat.format(date);
+        String url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=" + today +
+                "&end_date=" + today + "&api_key=47NucpwWv137CZPlAJZxykgMTMR9KF4bj5TzCYSG";
 
         JsonObjectRequest jsObjRequest = new JsonObjectRequest(
                 Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
@@ -43,7 +54,6 @@ public class HomeActivity extends AppCompatActivity {
 // JSON response​
             }
         }, new Response.ErrorListener() {
-
             @Override
             public void onErrorResponse(VolleyError error) {
                 Toast.makeText(HomeActivity.this, "Okay, Houston, we've had a problem here.", Toast.LENGTH_SHORT).show();
