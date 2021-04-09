4f333531ce52edb5454310212718141c9717fa0b
BastLast <bastien.last87@gmail.com>
2021-04-09 16:11:50 +0200

    added info to the info page

--- app/src/main/java/com/example/assstriode/ActivityAsteroidInfo.java
+++ app/src/main/java/com/example/assstriode/ActivityAsteroidInfo.java
@@ -5,8 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.assstriode.model.Asteroid;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static com.example.assstriode.R.string.fail_toast;
+import static java.lang.Float.parseFloat;
 
 public class ActivityAsteroidInfo extends AppCompatActivity {
 
@@ -27,15 +40,40 @@ public class ActivityAsteroidInfo extends AppCompatActivity {
         tvDistance.setText(getString(R.string.distance) + " " + asteroid.getDistance());
         tvMagnitude.setText(getString(R.string.magnitude) + " " + asteroid.getMagnitude());
 
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+
+        String url = generateUrl(asteroid.getId());
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest(
+                Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                System.out.println(response);
+                try {
+                    asteroid.setPeriodeOrbitale(parseFloat((String) response.getJSONObject("orbital_data").get("orbital_period")));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                tvPeriod.setText(getString(R.string.orbital) + " " + asteroid.getPeriodeOrbitale());
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(ActivityAsteroidInfo.this, fail_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(jsObjRequest);
 
     }
 
     /**
      * generate the url for api calls
      *
+     * @param id
      * @return
      */
-    private String generateUrl() {
-        return "https://api.nasa.gov/neo/rest/v1/neo/3726710?api_key=" + getResources().getString(R.string.apikey);
+    private String generateUrl(int id) {
+        return "https://api.nasa.gov/neo/rest/v1/neo/"+ id +"?api_key=" + getResources().getString(R.string.apikey);
     }
 }
